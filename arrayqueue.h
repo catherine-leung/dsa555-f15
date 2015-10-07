49a7eb20f9eea6793260ce24d828878ed8fc11f0
Catherine Leung <cathyatseneca@gmail.com>
2015-10-07 15:21:22 -0400

    array version of queue

--- arrayqueue.h
+++ arrayqueue.h
@@ -4,30 +4,44 @@ class Queue{
 	T* data_;
 	int maxSize_;
 	int currSize_;
+	int front_;
+	int back_;
 	void grow(){
 		T* tmp=new T[maxSize_*2];
 		for(int i=0;i<currSize_;i++){
-			tmp[i]=data_[i];
+			tmp[i]=front();
+			dequeue();
 		}
 		delete data_;
 		data_=tmp;
 		maxSize_=maxSize_*2;
+		front_=0;
+		back_=currSize_;
 	}
 public:
 	Queue(max=100){
 		data_=new T[max];
 		maxSize_=max;
 		currSize_=0;
+		front_=0;
+		back_=0;
 	}
 	void enqueue(const T& dat){
 		if(currSize_ == maxSize_){
 			grow();
 		}
-		
+		data_[back_]=dat;		
+		currSize_++;
+		back_=(back+1)%maxSize_;
 	}
 	void dequeue(){
+		if(currSize_){
+			front_=(front+1)%maxSize_;
+			currSize_--;
+		}
 	}
-	const T& top(){
+	const T& front(){
+		return data_[front_];
 	}
 	bool isEmpty() const;
 };
